cc53fcd82a63f2f89d9c95aad8353fc5157e69ed
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-04 01:57:24 +0300

    Create category repository

---
+++ RealEstate/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using RealEstate.Models.DapperContext;
+using RealEstate.Models.DTOs.CategoryDTOs;
+
+namespace RealEstate.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async void CreateCategoryAsync(CreateCategoryDto categoryDto)
+        {
+            string query = "Insert into Category (CategoryName,CategoryStatus) values (@categoryName, @categoryStatus)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryName", categoryDto.CategoryName);
+            parameters.Add("@categoryStatus", true);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteCategory(int id)
+        {
+            string query = "Delete from Category Where CategoryId=@categoryId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryId", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "Select * From Category";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async void UpdateCategory(UpdateCategoryDto categoryDto)
+        {
+            string query = "Update Category Set CategoryName=@categoryName,CategoryStatus=@categoryStatus Where CategoryId=@categoryId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryId", categoryDto.CategoryId);
+            parameters.Add("@categoryName", categoryDto.CategoryName);
+            parameters.Add("@categoryStatus", categoryDto.CategoryStatus);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+    }
+}
---
+++ RealEstate/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate.Models.DTOs.CategoryDTOs;
+
+namespace RealEstate.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        void CreateCategoryAsync(CreateCategoryDto categoryDto);
+        void DeleteCategory(int id);
+        void UpdateCategory(UpdateCategoryDto categoryDto);
+    }
+}
