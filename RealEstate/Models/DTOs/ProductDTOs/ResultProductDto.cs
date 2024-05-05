46295640e47ce371c8f9578fcd7c7cb3591ebfb3
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-05 14:43:58 +0300

    created productdto class

---
+++ RealEstate/Models/DTOs/ProductDTOs/ResultProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate.Models.DTOs.ProductDTOs
+{
+    public class ResultProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductTitle { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductCity { get; set; }
+        public string ProductDistrict { get; set; }
+        public int ProductCategory { get; set; }
+    }
+}
