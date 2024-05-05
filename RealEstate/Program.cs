51aef4d328a9b2514e399986a5305a2d39910111
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-05 14:45:13 +0300

    Product related dependencies resolved

--- RealEstate/Program.cs
+++ RealEstate/Program.cs
@@ -1,5 +1,6 @@
 using RealEstate.Models.DapperContext;
 using RealEstate.Repositories.CategoryRepository;
+using RealEstate.Repositories.ProductRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<Context>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
