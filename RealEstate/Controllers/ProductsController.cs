5618abf96f632c71f28558f8f32da9381e88316a
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-05 14:42:47 +0300

    ProductsController added

---
+++ RealEstate/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Repositories.ProductRepository;
+
+namespace RealEstate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var values = await _productRepository.GetAllProductAsync();
+            return Ok(values);
+        }
+        [HttpGet("GetAllProductsWithCategory")]
+        public async Task<IActionResult> GetAllProductsWithCategory()
+        {
+            var values = await _productRepository.GetAllProductWithCategoriesAsync();
+            return Ok(values);
+        }
+
+    }
+}
