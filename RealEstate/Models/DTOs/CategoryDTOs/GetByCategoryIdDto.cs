82469777e29e4271a7223a2ae6a2e977408ef4b5
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-05 14:43:38 +0300

    Added to my list by Category Id

---
+++ RealEstate/Models/DTOs/CategoryDTOs/GetByCategoryIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate.Models.DTOs.CategoryDTOs
+{
+    public class GetByCategoryIdDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
