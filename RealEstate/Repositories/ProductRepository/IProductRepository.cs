37884a03e3f0be50309c04f07923c5a5826c0c58
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-05 14:44:52 +0300

    Product-related operations were organized

---
+++ RealEstate/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealEstate.Models.DTOs;
+using RealEstate.Models.DTOs.ProductDTOs;
+
+namespace RealEstate.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoriesAsync();
+    }
+}
---
+++ RealEstate/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using RealEstate.Models.DapperContext;
+using RealEstate.Models.DTOs;
+using RealEstate.Models.DTOs.CategoryDTOs;
+using RealEstate.Models.DTOs.ProductDTOs;
+
+namespace RealEstate.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            string query = "Select * From Product";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoriesAsync()
+        {
+            string query =
+                "SELECT ProductId,ProductTitle,ProductPrice,ProductCity,ProductDistrict,CategoryName from Product inner join Category on Product.ProductCategory=Category.CategoryId";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
