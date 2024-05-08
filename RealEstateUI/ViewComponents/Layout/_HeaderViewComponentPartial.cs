de6281a588fc9c101823e701e95552639f5508d5
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-08 17:34:15 +0300

    fixed component partial

---
+++ RealEstateUI/ViewComponents/Layout/_HeaderViewComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateUI.WiewComponents.Layout
+{
+    public class _HeaderViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            {
+                return View();
+            }
+        }
+    }
+}
